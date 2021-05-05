3faf0f5026d5631592ad5c9a8f9ed8288afa7a89
ezerway <faq.ezerway@gmail.com>
2021-05-05 14:45:54 +0700

    Feature: Add shopify/app-bridge-react

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,34 @@
 import React, { Fragment } from 'react';
 import App from 'next/app';
 import Head from 'next/head';
+import { Provider } from '@shopify/app-bridge-react';
 import { AppProvider } from '@shopify/polaris';
 import translations from '@shopify/polaris/locales/en.json';
 import '@shopify/polaris/dist/styles.css';
-
-export default class MyApp extends App {
+class MyApp extends App {
     render() {
-        const { Component, pageProps } = this.props;
+        const { Component, pageProps, shopOrigin } = this.props;
+        const config = { apiKey: API_KEY, shopOrigin, forceRedirect: true };
         return (
             <Fragment>
                 <Head>
                     <title>Ezerway 2 Countdown</title>
                     <meta charSet="utf-8"/>
                 </Head>
-                <AppProvider i18n={translations}>
-                    <Component {...pageProps}/>
-                </AppProvider>
+                <Provider config={config}>
+                    <AppProvider i18n={translations}>
+                        <Component {...pageProps}/>
+                    </AppProvider>
+                </Provider>
             </Fragment>
         );
     }
 }
+
+MyApp.getInitialProps = async (({ ctx }) => {
+    return {
+        shopOrigin: ctx.query.shop
+    }
+})
+
+export default MyApp;
